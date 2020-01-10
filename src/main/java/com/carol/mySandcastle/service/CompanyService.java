8b64ef6204b332f426fd3eb16c9ef266091d3dc0
Carol <galvao.carolina@hotmail.com>
2020-01-10 15:42:49 -0300

    aula 25

--- src/main/java/com/carol/mySandcastle/service/CompanyService.java
+++ src/main/java/com/carol/mySandcastle/service/CompanyService.java
@@ -1,8 +1,9 @@
 package com.carol.mySandcastle.service;
 
 import com.carol.mySandcastle.repositories.CompanyRepository;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CompanyService {
 
     private CompanyRepository repository;
