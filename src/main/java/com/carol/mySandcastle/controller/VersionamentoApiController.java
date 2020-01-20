61823759796f9759c3cdf27aa2647c1d51aa3da1
Carol <galvao.carolina@hotmail.com>
2020-01-20 18:24:18 -0300

    aula 30- entendendo versionamento de api
    ou por URL ou pelo header

---
+++ src/main/java/com/carol/mySandcastle/controller/VersionamentoApiController.java
@@ -0,0 +1,33 @@
+package com.carol.mySandcastle.controller;
+
+import com.carol.mySandcastle.response.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class VersionamentoApiController {
+
+    @GetMapping(value = "v1/ola/{name}")
+    public ResponseEntity<String> helloNameV1 (@PathVariable("name") String name){
+        return ResponseEntity.ok(String.format("API v1: Hello %s!", name));
+    }
+
+    @GetMapping(value = "v2/ola/{name}")
+    public ResponseEntity<String> helloNameV2 (@PathVariable("name") String name){
+        return ResponseEntity.ok(String.format("API v2: Hello %s!", name));
+    }
+
+    @GetMapping(value = "ola/{name}", headers = "X-API-Version=v1")
+    public ResponseEntity<String> helloNameHeaderV1(@PathVariable("name") String name){
+        return ResponseEntity.ok(String.format("API Header v1: Hello %s", name));
+    }
+
+    @GetMapping(value = "ola/{name}", headers = "X-API-Version=v2")
+    public ResponseEntity<String> helloNameHeaderV2(@PathVariable("name") String name){
+        return ResponseEntity.ok(String.format("API Header v2: Hello %s", name));
+    }
+}
